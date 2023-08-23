7ae4f3b99828848a5982a9032bc1f8e22e2320e5
Eddod <edwinwesterberg44@gmail.com>
2023-08-23 17:26:52 +0200

    Added LoginCommand, handler, request and declared interface for JWTProvider

---
+++ Application/Abstractions/IJWTProvider.cs
@@ -0,0 +1,8 @@
+﻿using Domain.Entities.Customers;
+
+namespace Application.Abstractions;
+
+public interface IJWTProvider
+{
+    string Generate(Customer customer);
+}
---
+++ Application/Customers/Login/LoginCommandHandler.cs
@@ -0,0 +1,14 @@
+﻿using Application.Abstractions.Messaging;
+using Domain.Shared;
+using MediatR;
+
+namespace Application.Customers.Login;
+
+internal sealed class LoginCommandHandler : ICommandHandler<LoginCommand, string>
+{
+
+    Task<Result<string>> IRequestHandler<LoginCommand, Result<string>>.Handle(LoginCommand request, CancellationToken cancellationToken)
+    {
+        throw new NotImplementedException();
+    }
+}
